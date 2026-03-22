623caa026fa5d92d71a8c834b2936626a8151d2d
lennartheld17 <lennart.held17@gmail.com>
2026-03-22 16:50:45 +0100

    change

---
+++ working_with_numbers.py
@@ -0,0 +1,7 @@
+import math # Mathe-Bibliothek mit mathematischen Methoden (Funktionen)
+
+#print(round(2.9)) # Round-Funktion für das Runden
+#print(abs(-2.9)) # Absolutwert-Funktion um den Betrag einer Zahl zu bekommen
+
+print(math.ceil(2.2)) # Aufrunden
+print(math.floor(2.2)) # Abrunden
