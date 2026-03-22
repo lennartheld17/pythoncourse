2eb46e107fa5e4a22c064f3efa9df373a42177da
lennartheld17 <lennart.held17@gmail.com>
2026-03-22 17:06:27 +0100

    change

---
+++ type_conversion.py
@@ -0,0 +1,15 @@
+x = int(input("x: ")) # Mit int() kann die Eingabe in ein Integer umgewandelt werden
+y = x + 1
+
+print(f"x = {x}, y = {y}")
+
+#int(x)
+#float(x)
+#str(x)
+
+#bool(x) <- Besonderheit bei diesem Datentyp: das was hier drunter steht ist immer False z.B. bool(0) = False
+# Falsy
+# ""
+# 0
+# None
+
