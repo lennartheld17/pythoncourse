81fe36cb564e62cc6878b7ee083126de421f0b97
lennartheld17 <lennart.held17@gmail.com>
2026-03-22 16:47:36 +0100

    change

---
+++ numbers.py
@@ -0,0 +1,13 @@
+#x = 1 + 2j # a + bi (komplexe Zahl)
+
+print (10 + 3)
+print (10 - 3)
+print (10 * 3)
+print (10 / 3)
+print (10 // 3) # Integer Ergebnis
+print (10 % 3) # Rest einer Division
+print (10 ** 3) # Exponent -> hier 10^3 = 1000
+
+x = 10
+#x = x + 3
+#x += 3 -> Kurzschreibweise
