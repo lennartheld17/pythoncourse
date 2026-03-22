a0cd81850674389106f74101eb0091e2994234de
lennartheld17 <lennart.held17@gmail.com>
2026-03-22 16:11:31 +0100

    variables.py

---
+++ string.py
@@ -0,0 +1,8 @@
+course = "Python Programming"
+# len -> function to get the length of string
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:])
+print(course[:])
+print(course[:3])
