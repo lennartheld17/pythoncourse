cd1ad37e944f9bac1c0ed3cf6f59148d558e26d2
lennartheld17 <lennart.held17@gmail.com>
2026-03-23 14:23:27 +0100

    change

---
+++ iterables.py
@@ -0,0 +1,11 @@
+print(type(5)) #int
+print(type(range(5))) #range Typ (iterierbar)
+
+# Iterable
+#for x in range(5)
+
+#for x in "Python": #iterieren über einen String
+#    print(x)
+
+#for x in [1,2,3,4]: #iterieren über eine Liste
+#    print(x)
