650114a1059875a64231f4df2a77d650b9d945ec
lennartheld17 <lennart.held17@gmail.com>
2026-03-23 14:13:06 +0100

    change

--- for_else.py
+++ for_else.py
@@ -1,2 +1,9 @@
+successful = True
+
 for number in range(3):
     print("Attempt")
+    if successful:
+        print("Successful")
+        break #Aus dem for-loop herausgehen, wenn der Code ausgeführt wurde
+else:
+    print("Attempted 3 times and failed!")
